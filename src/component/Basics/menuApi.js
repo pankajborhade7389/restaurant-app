04b17fce2d32abd6c9ef10ffc8dd5d95c0f06eec
unknown <pankajborhade333@gmail.com>
2022-07-24 23:24:47 +0530

    react

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,95 @@
+const Menu = [
+    {
+      id: 1,
+       image: "https://5.imimg.com/data5/FW/RU/GLADMIN-64186824/veg-masala-maggi-500x500.png",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://vaya.in/recipes/wp-content/uploads/2018/03/Indori-Poha.jpg",
+      name: "Poha",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://w7.pngwing.com/pngs/796/941/png-transparent-corn-on-the-cob-five-grains-a-corn-soya-food-recipe-images.png",
+      name: "Corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://img.freepik.com/free-photo/indian-dish-spicy-chick-peas-curry-also-known-as-chola-chana-masala-commonly-chole-served-served-with-fried-puri-poori-colourful-wooden-background-selective-focus_466689-67484.jpg",
+      name: "Chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://img.favpng.com/2/20/6/california-style-pizza-sicilian-pizza-domino-s-pizza-food-png-favpng-5Kk8sz7nAmjSdn8tX8BqPm61B.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://media.istockphoto.com/photos/kolhapuri-chicken-thali-is-a-popular-indianasian-food-platter-of-picture-id981620744?k=20&m=981620744&s=170667a&w=0&h=SGlJ9I90zoDpq0Ca1131LIceTLTD1BTKIk2HbKN8cdk=",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://thumbs.dreamstime.com/b/stock-photo-indian-sweets-served-silver-wooden-plate-variety-peda-burfi-laddu-decorative-selective-focus-diwali-100924860.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://www.secondrecipe.com/wp-content/uploads/2017/08/rajma-chawal-1.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://w7.pngwing.com/pngs/24/236/png-transparent-triangular-fried-food-with-vegetables-south-indian-cuisine-samosa-paratha-wrap-samosa-miscellaneous-food-recipe.png",
+      name: "Samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "https://image.cookly.me/images/14-days-professional-chef-guided-indian-authentic-culinary-tour-in-gods-own-count_nwfdcFF.jpg",
+      name: "Idli Dosa",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
